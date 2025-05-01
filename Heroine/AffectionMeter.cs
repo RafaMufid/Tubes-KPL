5821f155e279469ba353c1b01d578c8fcc1a4792
RafaMufid <akurafamufid@gmail.com>
2025-05-01 22:37:12 +0700

    menambahkan kelas AffectionMeter

---
+++ Heroine/AffectionMeter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_KPL_Kel6.Heroine;
+
+class AffectionMeter
+{
+    private int affectionValue;
+    private Dictionary<int, string> affectionTable;
+
+    public AffectionMeter()
+    {
+        // Set default value
+        affectionValue = 50;
+
+        // Inisialisasi tabel affection
+        affectionTable = new Dictionary<int, string>
+        {
+            { 5, "I wanna kill this guy" },
+            { 30, "Hate" },
+            { 50, "Friendly" },
+            { 80, "Like" },
+            { 100, "In Love" }
+        };
+    }
+
+    public int GetValue()
+    {
+        return affectionValue;
+    }
+
+    public string GetLevel()
+    {
+        int bestMatch = -1;
+
+        foreach (var entry in affectionTable)
+        {
+            if (affectionValue >= entry.Key)
+            {
+                if (entry.Key > bestMatch)
+                {
+                    bestMatch = entry.Key;
+                }
+            }
+        }
+
+        if (bestMatch != -1)
+        {
+            return affectionTable[bestMatch];
+        }
+        else
+        {
+            return "Unknown";
+        }
+    }
+    
+    public void Increase(int amount)
+    {
+        affectionValue += amount;
+        if (affectionValue > 100)
+        {
+            affectionValue = 100;
+        }
+    }
+
+    public void Decrease(int amount)
+    {
+        affectionValue -= amount;
+        if (affectionValue < 5)
+        {
+            affectionValue = 5;
+        }
+    }
+}
--- obj/Debug/net8.0/Tubes-KPL-Kel6.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tubes-KPL-Kel6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tubes-KPL-Kel6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27caff44047f37d8d2d2cc81695bca57b73a1bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea50c63827e94a63648a239dc0eb85c030f239a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tubes-KPL-Kel6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tubes-KPL-Kel6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
