921234e8446e979f4eda38f5359af537646c31a7
Thoriq111 <thoriqmarcello2146@gmail.com>
2025-05-02 14:51:30 +0700

    menambahkan fitur merchant

---
+++ Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Weapon
+{
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} (Price: {Price})";
+    }
+}
